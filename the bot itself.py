52fac98ff4b4fd07fbd33c291d6182fd3daf321b
Mikhail Gerasimov <mihv2004@gmail.com>
2021-04-24 14:51:11 +0500

    .

--- the bot itself.py
+++ the bot itself.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler, ConversationHandler
 from transform_scripts.color_transform import color_transformation
 from transform_scripts.palette_transform import palette_transformation
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import os
 
 
@@ -14,7 +14,8 @@ def stop():
 
 
 def start(update, context):
-    update.message.reply_text('Привет, я бот-обработчик, можешь прислать мне фотографию и я её обработаю!')
+    update.message.reply_text('Привет, я бот-обработчик, можешь прислать мне фотографию и я её обработаю!',
+                              reply_markup=start_markup)
     return 1
 
 
@@ -25,7 +26,7 @@ def help(update, context):
 def receive_photo(update, context):
     context.user_data['file_id'] = update.message.photo[-1].file_id
     update.message.reply_text('Я получил фото, какую обработку ты хочешь получить?',
-                              reply_markup=markup)
+                              reply_markup=markup_receive_photo)
     return 1
 
 
@@ -43,7 +44,8 @@ def b_w_transform(update, context):
 
 
 def palette_chosen(update, context):
-    update.message.reply_text('Выбери палитру цветов')
+    update.message.reply_text('Выбери палитру цветов',
+                              reply_markup=markup_choose_palette)
     for palette in range(1, 3):
         context.bot.send_photo(update.message.chat_id, open(f'palettes/palette_{palette}.jpg', 'rb'))
     return 2
@@ -64,6 +66,8 @@ def palette_transform(update, context):
     if num != '':
         nums.append(int(num))
     if len(nums) == 1 and 'любая' not in text.lower() and 0 < nums[0] < 3:
+        update.message.reply_text('Понял, сделаю в течении минуты',
+                                  reply_markup=markup_receive_photo)
         user_id = update.message.from_user.id
         if os.path.exists(f'user_images/{user_id}.jpg'):
             os.remove(f'user_images/{user_id}.jpg')
@@ -109,6 +113,9 @@ def command_check(update, context):
             or 'nft' in text.lower() or 'token' in text.lower():
         update.message.reply_text(f'Хочешь узнать об NFT токенах и продавать свои творения? Ты сможешь почитать о нём здесь: {LINKS["nft"]}')
         return 3
+    elif 'умеешь' in text.lower():
+        help(update, context)
+        return 3
     else:
         update.message.reply_text('Я тебя не понял')
         update.message.reply_text('Ты можешь воспользоваться кнопками быстрых комманд, это легко и удобно!')
@@ -131,13 +138,14 @@ def main():
         entry_points=[MessageHandler(Filters.photo, receive_photo, pass_user_data=True),
                       MessageHandler(Filters.text, command_check, pass_user_data=True)],
         states={
-            1: [CommandHandler('b_w_transform', b_w_transform, pass_user_data=True),
-                CommandHandler('palette_chosen', palette_chosen, pass_user_data=True),
+            1: [CommandHandler('b_w', b_w_transform, pass_user_data=True),
+                CommandHandler('palette', palette_chosen, pass_user_data=True),
                 MessageHandler(Filters.text, phrase_check, pass_user_data=True)],
             2: [MessageHandler(Filters.text, palette_transform, pass_user_data=True)],
             3: [MessageHandler(Filters.photo, receive_photo, pass_user_data=True),
-                CommandHandler('b_w_transform', simple_reply),
-                CommandHandler('palette_chosen', simple_reply)]
+                CommandHandler('b_w', simple_reply),
+                CommandHandler('palette', simple_reply),
+                MessageHandler(Filters.text, command_check, pass_user_data=True)]
         },
         fallbacks=[MessageHandler(Filters.photo, receive_photo, pass_user_data=True)]
     )
@@ -149,6 +157,10 @@ def main():
 
 
 if __name__ == '__main__':
-    reply_keyboard = [['/b_w_transform', '/palette_chosen']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    reply_keyboard = [['Хочу чёрно-белую', 'Переведи в палитру цветов']]
+    markup_receive_photo = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    reply_keyboard = [['1', '2', '3'], ['4', '5', '6']]
+    markup_choose_palette = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    reply_keyboard = [['Обработай мне фото', 'Что ты умеешь?']]
+    start_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     main()
